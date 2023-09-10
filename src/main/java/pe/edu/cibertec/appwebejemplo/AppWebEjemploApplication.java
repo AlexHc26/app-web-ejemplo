c8f6c6a627b936e7e0854d6b86fbe5de5a02a441
AlexHc26 <mahc2601@gmail.com>
2023-09-10 10:37:13 -0500

    ejemplo commit

---
+++ src/main/java/pe/edu/cibertec/appwebejemplo/AppWebEjemploApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebejemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebEjemploApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/appwebejemplo/AppWebEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.appwebejemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
